61a0ff854301bf5258e0e650cc1146c85fc2f0b6
Joachim Happel <joachim.happel@gmail.com>
2021-12-08 13:38:53 +0100

    fixes

--- functions.php
+++ functions.php
@@ -4,4 +4,5 @@ if (! defined('WP_DEBUG')) {
 }
 add_action( 'wp_enqueue_scripts', function () {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style('old_styles', get_stylesheet_directory_uri().'/old-style.css');
 });
