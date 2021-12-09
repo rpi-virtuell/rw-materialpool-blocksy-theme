5cb163444ad4e55bebb8ba79b28532056496cc9a
Joachim Happel <joachim.happel@gmail.com>
2021-12-09 11:58:15 +0100

    add template overrides to theme

--- functions.php
+++ functions.php
@@ -10,7 +10,8 @@ include_once "inc/themeCore.php";
 
 add_action( 'wp_enqueue_scripts', function () {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-	wp_enqueue_style('old_styles', get_stylesheet_directory_uri().'/old-style.css');
+	wp_enqueue_style('old-styles', get_stylesheet_directory_uri().'/old-style.css');
+	wp_enqueue_style('theme-styles', get_stylesheet_directory_uri().'/style.css');
 });
 
 
--- materialpool/archive-organisation.php
+++ materialpool/archive-organisation.php
@@ -10,51 +10,36 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
-
-get_header( 'materialpool' ); ?>
-
-    <div class="wrap">
-        <div id="primary" class="content-area">
-            <main id="main" class="site-main" role="main">
-                <div class="wrap">
-                    <div id="primary" class="content-area">
-                        <main id="main" class="site-main" role="main">
-                            <div class="entry-content material-facet-search">
-                                <div class="material-facetscontainer">
-                                    <div class="mfacet-wrap">
-                                        <div class="first-search-facets">
-	                                        <?php echo facetwp_display( 'facet', 'alpika_organisation' ); ?>
-	                                        <?php echo facetwp_display( 'facet', 'konfession_organisation' ); ?>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="material-resultcontainer">
-                                    <div class="material-suche">
-										<?php echo facetwp_display( 'facet', 'organisation_suche' ); ?>
-                                    </div>
-                                    <div class="clear"></div>
-                                    <div class="material-selection"><?php echo facetwp_display( 'selections' ); ?></div>
-                                    <div>
-                                        <div class="material-counter">
-											<?php echo facetwp_display( 'counts' ); ?> Treffer
-                                        </div>
-                                        <div class="material-pager">
-											<?php echo facetwp_display( 'pager' ); ?>
-                                        </div>
-                                    </div>
-                                    <div class="clear"></div>
-                                    <div class="material-results"><?php echo facetwp_display( 'template', 'organisation' ); ?></div>
-                                    <div class="material-pager"><?php echo facetwp_display( 'pager' ); ?></div></div><div id="page-loader"></div>
-                            </div>
+ThemeCore::draw_page_pre_content();
+?>
+            <div class="entry-content material-facet-search">
+                <div class="material-facetscontainer">
+                    <div class="mfacet-wrap">
+                        <div class="first-search-facets">
+                            <?php echo facetwp_display( 'facet', 'alpika_organisation' ); ?>
+                            <?php echo facetwp_display( 'facet', 'konfession_organisation' ); ?>
+                        </div>
                     </div>
-            </main>
-        </div>
-    </div>
-    </main>
-    </div>
-    <div id="secondary" style="display: none"></div>
-    </div>
-<?php get_footer();
+                </div>
+                <div class="material-resultcontainer">
+                    <div class="material-suche">
+                        <?php echo facetwp_display( 'facet', 'organisation_suche' ); ?>
+                    </div>
+                    <div class="clear"></div>
+                    <div class="material-selection"><?php echo facetwp_display( 'selections' ); ?></div>
+                    <div>
+                        <div class="material-counter">
+                            <?php echo facetwp_display( 'counts' ); ?> Treffer
+                        </div>
+                        <div class="material-pager">
+                            <?php echo facetwp_display( 'pager' ); ?>
+                        </div>
+                    </div>
+                    <div class="clear"></div>
+                    <div class="material-results"><?php echo facetwp_display( 'template', 'organisation' ); ?></div>
+                    <div class="material-pager"><?php echo facetwp_display( 'pager' ); ?></div></div><div id="page-loader"></div>
+            </div>
 
 
-
+<?php
+ThemeCore::draw_page_post_content();
