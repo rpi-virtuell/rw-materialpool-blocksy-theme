3ee28e90a94d7e92b0e503de809aa8d6ec66db28
Joachim Happel <joachim.happel@gmail.com>
2021-12-09 15:31:21 +0100

    add functions

--- functions.php
+++ functions.php
@@ -80,3 +80,141 @@ function frontend_ajax() {
 if ( !is_admin() ) {
 	add_action( 'wp_head', 'frontend_ajax');
 }
+
+
+
+/**
+ * facetwp wpquery manipulationen
+ */
+
+function facetwp_query_args_autor( $query_args, $class ) {
+	global $post;
+
+	if(strpos($class->ajax_params['http_params']['uri'],'autor/') === false){
+		return $query_args;
+	}
+	if ( 'material_autor' == $class->ajax_params['template'] ) {
+
+		$autor = get_page_by_path( str_replace( 'autor/', '', $class->ajax_params['http_params']['uri'] ), OBJECT, 'autor' );
+
+		$material_ids = get_post_meta( $autor->ID, 'material_autoren', true );
+
+		if ( isset( $query_args['meta_query'] ) ) {
+			unset( $query_args['meta_query'] );
+		}
+
+		$query_args["post__in"] = $material_ids;
+	}
+
+	/*
+	if ( defined('REST_REQUEST') && REST_REQUEST ) {
+          if ( 'material_autor' == $class->ajax_params['template'] ) {
+            $autor =  get_page_by_path( str_replace('autor/','',$class->ajax_params['http_params']['uri']) , OBJECT, 'autor' );
+            $query_args['meta_query'][0][ 'value'] = (string)$autor->ID;
+        }
+    } else {
+        if ( 'material_autor' == $class->ajax_params['template'] ) {
+
+            $query_args['meta_query'][0][ 'value'] = (string)$post->ID;
+        }
+    }
+	*/
+	return $query_args;
+}
+
+add_filter( 'facetwp_query_args', 'facetwp_query_args_autor', 10, 2 );
+
+function facetwp_query_args_organisation( $query_args, $class ) {
+
+	if (defined('REST_REQUEST') && REST_REQUEST) {
+
+		if ('material_organisation' == $class->ajax_params['template']) {
+			$organisation =  get_page_by_path( str_replace('organisation/','',$class->ajax_params['http_params']['uri']) , OBJECT, 'organisation' );
+			$query_args['meta_query'][0][ 'value'] = (string)$organisation->ID;
+		}
+	} else {
+		global $post;
+		if ('material_organisation' == $class->ajax_params['template']) {
+			$query_args['meta_query'][0]['value'] = $post->ID;
+		}
+	}
+	return $query_args;
+}
+add_filter( 'facetwp_query_args', 'facetwp_query_args_organisation', 10, 2 );
+
+function facetwp_query_args_werkzeug( $query_args, $class ) {
+
+	if (is_tax() ) {
+		$term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
+		$query_args['tax_query'][0]['terms'] = $term->slug;
+	}
+	return $query_args;
+}
+
+add_filter( 'facetwp_query_args', 'facetwp_query_args_werkzeug', 10, 2 );
+
+
+function facetwp_query_args_material_verweise( $query_args, $class ) {
+	global $post;
+	if ('material_verweise' == $class->ajax_params['template']) {
+		$query_args = Materialpool_Material::get_verweise_ids();
+	}
+	return $query_args;
+}
+add_filter( 'facetwp_pre_filtered_post_ids', 'facetwp_query_args_material_verweise', 10, 2 );
+
+function facetwp_query_args_themenseiten( $query_args, $class ) {
+	global $post;
+	global $themenseite_material_id_list;
+
+	$func = function($value) { return (int) $value;};
+
+	$material_id_list = array();
+	if (defined('REST_REQUEST') && REST_REQUEST) {
+
+		if ('thema' == $class->ajax_params['template']) {
+			$themenseite =  get_page_by_path( str_replace('themenseite/','',$class->ajax_params['http_params']['uri']) , OBJECT, 'themenseite' );
+
+
+			foreach(Materialpool_Themenseite::get_gruppen($themenseite->ID) as $gruppe){
+				$id_list = array_map( $func, $gruppe[ 'auswahl'] );
+				$material_id_list = array_merge($material_id_list, $id_list);
+			}
+
+			$query_args = $material_id_list;
+		}
+
+	}elseif ($post->post_type == "themenseite" && !is_embed() ){
+
+		foreach(Materialpool_Themenseite::get_gruppen($post->ID) as $gruppe){
+			if ( ! $gruppe[ 'auswahl'] ) continue;
+			$id_list = array_map( $func, $gruppe[ 'auswahl']  );
+			$material_id_list = array_merge($material_id_list, $id_list);
+		}
+		$query_args = $material_id_list;
+	}
+	return $query_args;
+}
+add_filter( 'facetwp_pre_filtered_post_ids', 'facetwp_query_args_themenseiten', 10, 2 );
+
+
+
+
+
+
+
+function my_facetwp_facet_html( $output, $params ) {
+	if ( 'alpika' == $params['facet']['name'] ) {
+		$output = str_replace('>1 <','>aus den Instituten <',$output);
+	}
+	if ( 'alpika_organisation' == $params['facet']['name'] ) {
+		$output = str_replace('>1 <','>ALPIKA <',$output);
+	}
+	if ( 'erscheinungsjahr' == $params['facet']['name'] ) {
+		$output = str_replace('>0 <','>Keine Angabe <',$output);
+	}
+
+	return $output;
+}
+
+add_filter( 'facetwp_facet_html', 'my_facetwp_facet_html', 10, 2 );
